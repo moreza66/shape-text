aaff02534b5b4c7ba32d2a39341135288e790b00
“momemar” <mmemar@logisx.net>
2023-10-20 12:55:53 -0500

    create a function

--- Alphabet-Triangle-17.js
+++ Alphabet-Triangle-17.js
@@ -6,20 +6,7 @@
 // D D D D
 // E E E E E
 
-function printPattern() {
-    for (let i = 1; i <= 5; i++) {
-      const letter = String.fromCharCode(64 + i); // Convert to A, B, C, etc.
-      let row = '';
-  
-      // Create the row content based on the value of i
-      for (let j = 1; j <= i; j++) {
-        row += letter;
-      }
-  
-      console.log(row); // Display the row
-    }
-  }
-  printPattern();
-
-
+function alphabetTriangle () {
+    
+}
   
