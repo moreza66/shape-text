bfa24965aca28381b05f44257ad69d07e0e563b3
“momemar” <mmemar@logisx.net>
2023-10-20 13:05:32 -0500

    create a row content and call it

--- Alphabet-Triangle-17.js
+++ Alphabet-Triangle-17.js
@@ -7,6 +7,16 @@
 // E E E E E
 
 function alphabetTriangle () {
-    
-}
+    for (let i = 1; i <= 5; i++){
+        const letter = String.fromCharCode(64 + i);
+        let row = '';
+        for (let j = 1; j <= i; j++) {
+          row += letter;
+        }
+        console.log(row);
+      }
+    }
+    alphabetTriangle ();
+
+
   
