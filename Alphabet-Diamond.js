794811a5dfecdfaa141f028e6b972874290f9f5a
Bahman Azimi <80485882+B-Azimi@users.noreply.github.com>
2023-10-20 10:02:13 +0300

    Update Alphabet-Diamond.js

--- Alphabet-Diamond.js
+++ Alphabet-Diamond.js
@@ -2,15 +2,20 @@ const numRows = 5;
 const alphabet = 'ABCDEFGHI';
 
 Array(numRows).fill().forEach((_, i) => {
-  const row = Array(numRows - i).fill(' ').join('') + Array(2 * i + 1).fill().map((_, j) => alphabet[j]).join('');
+  const spaces= Array(numRows - i).fill(' ').join('')
+  const stars= Array(2 * i + 1).fill().map((_, j) => { return alphabet[j] }).join('')
+  const row = spaces + stars + spaces;
   console.log(row);
  
 });
 {
 const numRows = 4;
 const alphabet = 'ABCDEFG';
+const length= alphabet.length;
 Array(numRows).fill().forEach((_, i) => {
-  const rowrevers = Array(numRows - 2 + i).fill(' ').join('') + Array(7 - 2 * i).fill().map((_, j) => alphabet[j]).join('');
-  console.log(rowrevers);
+   const spaces= Array(numRows - 2 + i).fill(' ').join('')
+   const stars=  Array(length - 2 * i).fill().map((_, j) =>{ return alphabet[j]}).join('')
+   const rowreverse = spaces + stars + spaces;
+   console.log(rowreverse);
 });
 }
